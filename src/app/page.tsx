f02b3df8c9a55fce21298862afc99045c35fa043
Miguel Habana <mighabana@gmail.com>
2024-09-21 11:10:48 +0200

    Update initial design for the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,88 @@
+'use client'
+
 import Image from "next/image";
+import { fraunces } from '@/app/fonts';
+import { useEffect, useState } from "react";
+
+function Description({n} : {n:number}){
+  const options = [{
+    name: 'data professional',
+    color: '#FF8400',
+    image1: '/analytics.png',
+    image2: '/database.png',
+    image3: '/code.png'
+  },{
+    name: 'photographer',
+    color: '#0084FF',
+    image1: '/jpeg.png',
+    image2: '/portrait.png',
+    image3: '/cute-camera.png'
+  },{
+    name: 'music lover',
+    color: '#2BFF00',
+    image1: '/music.png',
+    image2: '/romantic-music.png',
+    image3: '/listen.png'
+  }]
+
+  return (
+    <div className='flex flex-row pl-[75%] w-max'>          
+      <Image className="relative right-5 -rotate-[20deg]" src={options[n].image1} width={50} height={50} alt=""/>
+      <h2>a <span className={`text-[${options[n].color}]`}>{options[n].name}</span>.</h2>
+      <Image className="relative top-14 right-24" src={options[n].image2} width={50} height={50} alt=""/>
+      <Image className="relative bottom-14 right-16 rotate-[25deg]" src={options[n].image3} width={50} height={50} alt=""/>
+    </div>
+  );
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  let [n, setN] = useState(0)
+  let [loading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    const handleWheel = (event) => {
+      event.preventDefault()
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      if (!loading && Math.abs(event.deltaY) < 6) {
+        setLoading(true)
+        console.log(event.deltaY)
+
+        if ( event.deltaY > 0) {
+          if (n + 1 > 2) {
+            setN(0)
+          } else {
+            setN(n + 1)
+          }
+        } else if ( event.deltaY < 0) {
+          if (n - 1 < 0) {
+            setN(2)
+          } else {
+            setN(n-1)
+          }
+        }
+        
+        window.setTimeout(() => {setLoading(false)} ,1500)
+      }
+
+    };
+
+    // Add the event listener when the component mounts
+    window.addEventListener('wheel', handleWheel, {passive : false});
+
+    // Cleanup the event listener when the component unmounts (if it happens before the first event)
+    return () => {
+      window.removeEventListener('wheel', handleWheel,);
+    };
+  }, [n, loading]);
+
+  return (
+    <div className="flex flex-col items-center justify-center justify-items-center min-h-screen text-2xl">
+      <div className={`${fraunces.className} flex flex-col justify-center items-center min-w-2/3`}>
+        <div className='pr-[55%] pb-4 w-max'>
+          <h1>Hi! I'm Miguel Habana...</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <Description n={n}/>
+      </div>
     </div>
   );
 }
